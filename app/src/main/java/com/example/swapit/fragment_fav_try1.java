dbe78d0b0b59f6c8555a950d620ee411adc37669
mariemgharsallah <maryemgharssalla@gmail.com>
2024-04-26 03:00:53 +0100

    maryem try

---
+++ app/src/main/java/com/example/swapit/fragment_fav_try1.java
@@ -0,0 +1,14 @@
+package com.example.swapit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_fav_try1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favourites_fragment);
+    }
+}
